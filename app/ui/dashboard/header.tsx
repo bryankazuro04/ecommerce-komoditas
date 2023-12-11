bf5dba0c103962c7a48a58ba624de0a38e271177
bryankazuro04 <mhdalana211201@gmail.com>
2023-12-11 21:45:57 +0700

    fixing header nav

--- app/ui/dashboard/header.tsx
+++ app/ui/dashboard/header.tsx
@@ -4,7 +4,7 @@ import Avatar from "@mui/material/Avatar";
 export default function HeadNav() {
   return (
     <>
-      <Container maxWidth="lg" className="flex items-center">
+      <Container maxWidth="lg" className="!flex items-center">
         <h1>test</h1>
 
         <Avatar className="ml-auto">A</Avatar>
